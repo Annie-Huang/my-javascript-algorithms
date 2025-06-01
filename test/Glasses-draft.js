9490f7e25c113d30d8f6609681e343920a85a41d
Annie Huang <annie.hn@gmail.com>
2025-06-01 21:32:19 +1000

    Add Glasses-draft.js

---
+++ test/Glasses-draft.js
@@ -0,0 +1,28 @@
+/*
+  Need to run against data, not sure if I am right.
+
+  (N=20, K=10)
+  (N=10, K=10)
+  (N=4, K=10)
+  1.
+  if(N>K) {
+    N = K;
+  }
+
+  count = 0;
+  finish = false;
+  while(K > 0) {
+    if(N => K) {
+      count++
+      return count;
+    } else if (N < K) {
+      if(N <= 0) {
+        return count;
+      }
+      count++;
+      K = K-N;
+      N = N-1;
+    }
+  }
+
+ */
