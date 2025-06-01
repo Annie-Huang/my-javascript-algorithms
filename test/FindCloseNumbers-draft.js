f3c3f0dd2d5309d706376eec9281180509593062
Annie Huang <annie.hn@gmail.com>
2025-06-01 20:28:37 +1000

    Add FindCloseNumbers-draft.js

---
+++ test/FindCloseNumbers-draft.js
@@ -0,0 +1,15 @@
+/*
+  1.
+  newArr = A.sort((a, b) => b-a); to get descending order.
+
+  2.
+  if (N = 1) return false;
+
+  for (let i = 0; i < N-1; i++) {
+    if(newArr[i] - newArr[i+1] == 1) {
+      return true;
+    }
+  }
+  return false;
+
+ */
