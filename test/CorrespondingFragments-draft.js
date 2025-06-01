e2c0f85a92524abba4449cc9adc9656ac057c02a
Annie Huang <annie.hn@gmail.com>
2025-06-01 11:33:18 +1000

    Add CorrespondingFragments-draft.js

---
+++ test/CorrespondingFragments-draft.js
@@ -0,0 +1,26 @@
+/*
+ Two for loops.
+
+ let's say N = 10;
+
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; i++) {
+      compare position:
+      0-0
+      0-1
+      0-2
+      0-3
+      ...
+      0-9
+      1-1
+      1-2
+      1-3
+      ...
+      1-9
+
+      Create a map obj to compare, and add count if it satisfy condition.
+    }
+  }
+
+  pay attention to edge case in both end.
+ */
