edf97478bf6075205f96ab16819a4b64043bf070
Annie Huang <annie.hn@gmail.com>
2025-06-01 17:29:27 +1000

    Add DistinctArrayGenerator-draft.js

---
+++ test/DistinctArrayGenerator-draft.js
@@ -0,0 +1,18 @@
+/*
+  count = Math.floor(N/2)
+
+  if N is evenNumber: use mod to decide.
+    arr = []
+    for (let i = 1; i <= count; i++) {
+        arr.push(i);
+        arr.push(-i);
+    }
+  else
+    arr = [0]
+    for (let i = 1; i <= count; i++) {
+        arr.push(i);
+        arr.push(-i);
+    }
+
+   return arr
+*/
