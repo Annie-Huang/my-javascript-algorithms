93e0158d33ba90f26246ceb6f6059f1b52ccb0c0
Annie Huang <annie.hn@gmail.com>
2025-06-01 20:19:37 +1000

    Add EvenWord-draft.js

---
+++ test/EvenWord-draft.js
@@ -0,0 +1,14 @@
+/*
+  acbcbba;
+  1. create obj with key vs count:
+  {
+    a: 2;
+    b: 3;
+    c: 2
+  }
+
+  2. count = 0;
+  loop the Object.keys(obj), for each key value that is add, count++;
+  return count;
+   
+ */
