388919b80ab90b9456e3f59d41537d528c71312c
Annie Huang <annie.hn@gmail.com>
2025-06-01 13:47:41 +1000

    Add CreatePalindrome-draft.js

---
+++ test/CreatePalindrome-draft.js
@@ -0,0 +1,12 @@
+/*
+  newStr = str..split('').reverse().join('')
+
+  for (let i = 0; i < N; i++) {
+    compare each position,
+    if different return No
+    if ? && value, ? replace with value;
+    if value && ?, ? replace with value;
+    if ? && ?, ? replace with a
+  }
+
+ */
