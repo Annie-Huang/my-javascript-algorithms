9ed7cb408333997e3e6fc5fe036da1a0f7a7d927
Annie Huang <annie.hn@gmail.com>
2025-06-02 20:26:40 +1000

    Add Signum-draft.js

---
+++ test/Signum-draft.js
@@ -0,0 +1,10 @@
+/*
+    1. create a obj of count, loop through the array,
+       if A[i] == 0, return 0;
+       if A[i] > 0, do nothing;
+       if A[i] < 0, add to count:
+       {
+          -1: N
+       }
+       return N % 2 == 1 ? -1 : 1
+ */
