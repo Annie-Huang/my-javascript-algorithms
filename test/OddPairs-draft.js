ac5a1c102742d9bcd4cf95b11ec98ec3a080bbc3
Annie Huang <annie.hn@gmail.com>
2025-06-02 16:00:39 +1000

    Add OddPairs-draft.js

---
+++ test/OddPairs-draft.js
@@ -0,0 +1,13 @@
+/*
+  1.create oddArr = []; evenArr = [];
+    for (let i = 0; i < N; i++) {
+      if (Math.abs(A[i]) % 2 == 0) {
+        evenArr.push(A[i]);
+      } else {
+        oddArr.push(A[i])
+      }
+    }
+
+  2. return evenArr.length == oddArr.length
+
+ */
