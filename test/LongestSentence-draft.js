89253190117a8b552a23d9c07e5f0815e0785ee6
Annie Huang <annie.hn@gmail.com>
2025-06-02 10:42:12 +1000

    Add LongestSentence-draft.js

---
+++ test/LongestSentence-draft.js
@@ -0,0 +1,13 @@
+/*
+    1. Replace ?, ! with '.'
+
+    2. Split with '.', get sentenceArr[]
+
+    3. trim() to cut the leading space and ending space.
+
+    4. loop through sentenceArr for each element, split with ' ' + filter out element that == '', place it into sentenceArr2[]
+
+    5. for each element in sentenceArr2[], count and find out the max Count.
+
+    6. Test edge case of one sentence.
+ */
